4229ba2ef89e1cfe96701d5deba274ad2ee289a5
FredrikAlHam <43142824+FredrikAlHam@users.noreply.github.com>
2021-03-18 11:44:12 +0100

    Namespaced Package

--- Controls.cs
+++ Controls.cs
@@ -4,37 +4,43 @@ using Newtonsoft.Json;
 using UnityEngine;
 using System.IO;
 
-public static class Controls
+namespace PrincessApollo
 {
-    public static ControlScheme Scheme
+    namespace Controls
     {
-        get
+        public static class Controls
         {
-            if (scheme != null) return scheme;
-            else
+            public static ControlScheme Scheme
             {
-                var path = UnityEngine.Application.dataPath + "/controlls.ctrl";
-                if (!File.Exists(path))
+                get
                 {
-                    File.WriteAllText(path, "{\"keys\": {\"PlayerOne-Forward\": \"W\",\"PlayerOne-Back\": \"S\",\"PlayerOne-Left\": \"A\",\"PlayerOne-Right\": \"D\",\"PlayerOne-Punch\": \"LeftAlt\",\"PlayerTwo-Forward\": \"UpArrow\",\"PlayerTwo-Back\": \"DownArrow\",\"PlayerTwo-Left\": \"LeftArrow\",\"PlayerTwo-Right\": \"RightArrow\",\"PlayerTwo-Punch\": \"RightAlt\"}}");
-                    Debug.Log(path + " has been created");
-                }
-                scheme = JsonConvert.DeserializeObject<ControlScheme>(File.ReadAllText(path));
-                return scheme;
-            }
+                    if (scheme != null) return scheme;
+                    else
+                    {
+                        var path = UnityEngine.Application.dataPath + "/controls.ctrl";
+                        if (!File.Exists(path))
+                        {
+                            File.WriteAllText(path, "{\"keys\": {\"PlayerOne-Forward\": \"W\",\"PlayerOne-Back\": \"S\",\"PlayerOne-Left\": \"A\",\"PlayerOne-Right\": \"D\",\"PlayerOne-Punch\": \"LeftAlt\",\"PlayerTwo-Forward\": \"UpArrow\",\"PlayerTwo-Back\": \"DownArrow\",\"PlayerTwo-Left\": \"LeftArrow\",\"PlayerTwo-Right\": \"RightArrow\",\"PlayerTwo-Punch\": \"RightAlt\"}}");
+                            Debug.Log(path + " has been created");
+                        }
+                        scheme = JsonConvert.DeserializeObject<ControlScheme>(File.ReadAllText(path));
+                        return scheme;
+                    }
 
+                }
+                internal set
+                {
+                    scheme = value;
+                }
+            }
+            private static ControlScheme scheme;
         }
-        internal set
+        [System.Serializable]
+        public class ControlScheme
         {
-            scheme = value;
+            [JsonProperty]
+            public Dictionary<string, string> keys;
+            public ControlScheme(Dictionary<string, string> keys) => this.keys = keys;
         }
     }
-    private static ControlScheme scheme;
-}
-[System.Serializable]
-public class ControlScheme
-{
-    [JsonProperty]
-    public Dictionary<string, string> keys;
-    public ControlScheme(Dictionary<string, string> keys) => this.keys = keys;
 }
